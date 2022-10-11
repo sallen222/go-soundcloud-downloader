39f2b8b76397deaf98a5ea643b687ad09102cee2
sallen222 <222sallen@gmail.com>
2022-10-10 20:09:28 -0400

    added support for multiple tracks via csv

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module sc-download
 go 1.19
 
 require (
+	github.com/dhowden/tag v0.0.0-20220618230019-adf36e896086 // indirect
 	github.com/grafov/m3u8 v0.11.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/zackradisic/soundcloud-api v0.1.8 // indirect
--- main.go
+++ main.go
@@ -6,41 +6,87 @@ import(
 	"github.com/zackradisic/soundcloud-api"
 	"log"
 	"os"
+	"encoding/csv"
+	"io"
 )
 
 func main() {
-	url := flag.String("url", "", "the url for the song you want to download")
+	csv := flag.String("csv", "songs.csv", "the csv file you want to read from")
 	flag.Parse()
-	_ = url
+	_ = csv
 	
+	readCSV(*csv)
+
 	sc, err := soundcloudapi.New(soundcloudapi.APIOptions{})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 	fmt.Println(sc.ClientID())
+	for index := range songs{
+		url := songs[index].url
+		fmt.Println(url)
+		if soundcloudapi.IsURL(url, true, true) {
+			tracks, err := sc.GetTrackInfo(soundcloudapi.GetTrackInfoOptions{
+				URL: url,
+			})
+			if err != nil {
+				log.Fatal(err.Error())
+			}
 
-	if soundcloudapi.IsURL(*url, true, true) {
-		tracks, err := sc.GetTrackInfo(soundcloudapi.GetTrackInfoOptions{
-			URL: *url,
-		})
-		if err != nil {
-			log.Fatal(err.Error())
+			for index2 := range tracks {
+				fileName := songs[index].title + ".mp3"
+
+				out, err := os.Create(fileName)
+
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+				defer out.Close()
+
+				err = sc.DownloadTrack(tracks[index2].Media.Transcodings[0], out)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+			}
+		} else {
+			fmt.Println("URL is invalid")
 		}
-		fileName := tracks[0].Title + ".mp3"
+	}	
+}
 
-		out, err := os.Create(fileName)
+type song struct {
+	title string
+	artist string
+	url string
+}
 
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-		defer out.Close()
+var songs []song
 
-		err = sc.DownloadTrack(tracks[0].Media.Transcodings[0], out)
-		if err != nil {
-			log.Fatal(err.Error())
-		}	
-	} else {
-		fmt.Println("URL is invalid")
+func readCSV(fileName string) error{
+	csvFile, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println(err)
 	}
+	defer csvFile.Close()
+
+	reader := csv.NewReader(io.Reader(csvFile))
+	reader.FieldsPerRecord = 3
+
+	for{
+		line, error := reader.Read()
+		
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			return error
+		}
+
+		title := line[0]
+		artist := line[1]
+		url := line[2]
+		
+		songs = append(songs, song{title, artist, url})
+	}
+	return nil
 }
